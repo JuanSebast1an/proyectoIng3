c1b69102ad52336c01882ad9551b0f136a2ac3a6
JuanSebast1an <juansechacon31859@gmail.com>
2022-12-11 12:35:58 -0500

    proyecto casi final

--- ProyectoFinal/src/app/login/login-routing.module.ts
+++ ProyectoFinal/src/app/login/login-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { ChildrenOutletContexts, RouterModule, Routes } from '@angular/router';
 import { FormularioComponent } from './formulario/formulario.component';
+import { RecuperarComponent } from './recuperar/recuperar.component';
 const routes: Routes = [
   {
     path: 'login', component: FormularioComponent
+  },
+  {
+    path:'recuperarContraseña',
+    component: RecuperarComponent
   }
 ];
 
--- ProyectoFinal/src/app/login/login.module.ts
+++ ProyectoFinal/src/app/login/login.module.ts
@@ -3,11 +3,13 @@ import { FormularioComponent } from './formulario/formulario.component';
 import { LoginRoutingModule } from './login-routing.module';
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { RecuperarComponent } from './recuperar/recuperar.component';
 
 
 @NgModule({
   declarations: [
-    FormularioComponent
+    FormularioComponent,
+    RecuperarComponent
   ],
   imports: [
     CommonModule,
